176ca95aca6995f7162a67429cb5e46441c590f8
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-05-19 10:13:45 -0400

    Minor corrections

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-  const elementEvent = target;
-  elementEvent.addEventListener(eventName,fn);
+  // const elementEvent = target;
+  target.addEventListener(eventName,fn);
 }
 
 /*
