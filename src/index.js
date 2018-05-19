7ac47c10e8505c8b986d1a6f42a6349d49addbef
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-05-19 10:16:24 -0400

    1st improved

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-  // const elementEvent = target;
   target.addEventListener(eventName,fn);
 }
 
